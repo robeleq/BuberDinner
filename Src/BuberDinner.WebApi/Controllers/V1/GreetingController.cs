21edea5236c13633c4096d6baf1e597e29691673
Robel Equbasilassie <robiki4life@gmail.com>
2023-09-15 15:31:08 +0300

    feat: Add a Greeting Controller

---
+++ Src/BuberDinner.WebApi/Controllers/V1/GreetingController.cs
@@ -0,0 +1,29 @@
+﻿using BuberDinner.Application.CmdQueryHandlers.Identity;
+using BuberDinner.Application.DTOs.Identity.Roles;
+using ErrorOr;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.WebApi.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/greetings")]
+    [AllowAnonymous]
+    public class GreetingController : BaseApiController
+    {
+        public GreetingController()
+        {
+          
+        }
+
+        [HttpGet("morning")]
+        public ActionResult GetMorningMessages(CancellationToken cancellationToken)
+        {
+            return Ok("Good Morning From ASP .NET Core 6.0");
+        }
+
+    }
+}
